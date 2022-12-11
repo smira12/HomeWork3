7112aa31c6801ac060f7461254b0aa432ca8726e
smira12 <m1ronsa89@gmail.com>
2022-12-11 16:14:13 +0300

    Add Task №23

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели не число. Введите число");
+        }
+    }
+
+    return result;
+}
+
+void PrintSquareTable(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write($"{i * i * i},");
+    }
+}
+
+int number = GetNumber("Введите число: ");
+
+PrintSquareTable(number);
